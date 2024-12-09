8b62d46fa52be7448e5b97031308fc98791c749d
Rafael Trindade <rafatrindade.exe@gmail.com>
2024-12-08 22:54:23 -0300

    Update 1_📈_Visão_Geral.py
    att

--- 1_📈_Visão_Geral.py
+++ 1_📈_Visão_Geral.py
@@ -1,7 +1,7 @@
-import streamlit as st # type: ignore
-import pandas as pd # type: ignore
-import plotly.express as px # type: ignore
-import plotly.graph_objects as go # type: ignore
+import streamlit as st
+import pandas as pd
+import plotly.express as px 
+import plotly.graph_objects as go 
 import numpy as np
 from datetime import datetime
 import datetime as dt
