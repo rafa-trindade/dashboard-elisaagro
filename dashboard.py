6346b8e133b19afd34df071dd11ab9bc9aa47a8d
Rafael Trindade <rafatrindade.exe@gmail.com>
2025-09-21 22:14:07 -0300

    att

--- dashboard.py
+++ dashboard.py
@@ -13,7 +13,7 @@ import src.utils as util
 # ---------------------------------------------------------------------
 st.set_page_config(
     layout="wide",
-    page_title="B2B Refeições | Elisa Agro", 
+    page_title="MH Refeições | Elisa Agro", 
     initial_sidebar_state="expanded", 
     page_icon="📊")
 
